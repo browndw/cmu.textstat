2658ded945ac8f7d374694bc4034e7bcba2dec99
Alex Reinhart <alex@refsmmat.com>
2025-08-27 15:37:50 -0400

    Document the sentiment_data dataset

---
+++ R/sentiment.R
@@ -0,0 +1,6 @@
+#' Novels for sentiment analysis
+#'
+#' Contains the full text of *Madame Bovary*, *A Portrait of the Artist as a
+#' Young Man*, *Ragged Dick*, and *The Rise of Silas Lapham*.
+#' @format readtext data frame with 4 observations
+"sentiment_data"
