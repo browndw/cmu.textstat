a1506397ad32eb802516b9ec817d6a5029b3b9eb
Alex Reinhart <alex@refsmmat.com>
2025-08-27 15:28:09 -0400

    Improve utility function docs

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -1,16 +1,19 @@
-#' This is a helper function for reading in the dialogue from Shakespeare plays
-#' and screenplays, which are formatted in very simple markup.
-#' With this function, either the dialogue or the direction can be
-#' execrated in bulk. It can also be used to extract the dialogue of specific characters.
-#' @param plays A readtext dataframe containing a doc_id column and a text column.
-#' @param extract A character vector specifying what is to be extracted like 'dialogue' or 'romeo'.
+#' Read dialogue and stage directions from Shakespeare plays
+#'
+#' The Shakespeare plays and screenplays are formatted in very simple markup.
+#' With this function, either the dialogue or the direction can be execrated in
+#' bulk. It can also be used to extract the dialogue of specific characters.
+#' @param plays A readtext dataframe containing a doc_id column and a text
+#'   column.
+#' @param extract A character vector specifying what is to be extracted:
+#'   'dialogue' or 'direction' for dialogue or stage directions, or the name of
+#'   a character (e.g., 'romeo') for one character's dialogue only.
 #' @return A readtext dataframe with the extracted text.
 #' @export
-from_play <- function(plays, extract = c("dialogue", "direction")) {
-  
-  if (missing(extract)) stop("You must specify whether you want to extract dialogue or direction.")
-  
+from_play <- function(plays, extract) {
+
   extract <- tolower(extract)
+
   # A function for reading in the simplified markup and extracting all the text
   # between dialogue nodes.
   get_text <- function(x) {
@@ -20,10 +23,9 @@ from_play <- function(plays, extract = c("dialogue", "direction")) {
     d <- stringr::str_squish(d)
     return(d)
   }
-  
+
   idx <- 1:nrow(plays)
   plays$text <- sapply(idx, function(i) paste(get_text(plays$text[i]), collapse = "\n"))
   plays <- plays[plays$text != "",]
   return(plays)
 }
-
